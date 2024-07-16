13cb1b3242e9ff7655bd92c3ce092f68d5d03986
Newtonkamau14 <newtonkamau14@gmail.com>
2024-07-16 22:29:31 +0300

    add layout components

---
+++ client/src/pages/AdminLayout.tsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom";
+
+function AdminLayout() {
+  return (
+    <>
+      <div>
+        <Outlet />
+      </div>
+    </>
+  );
+}
+
+export default AdminLayout;
