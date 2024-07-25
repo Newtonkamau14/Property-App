d3e348e2ac697ca0e1e9031c5680368acbc5b55d
Newtonkamau14 <newtonkamau14@gmail.com>
2024-07-25 22:31:58 +0300

    add client layout

--- client/src/pages/ClientLayout.tsx
+++ client/src/pages/ClientLayout.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router-dom";
-import Navbar from "../components/Navbar";
+import Navbar  from "../components/Navbar";
 import Footer from "../components/Footer";
+
 
 function ClientLayout() {
   return (
