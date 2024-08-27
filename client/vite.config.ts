4adf927d90d50b49934691208b2630bc28c05301
Newtonkamau14 <newtonkamau14@gmail.com>
2024-08-27 22:35:13 +0300

    server env variable

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({mode}) => {
+  const env = loadEnv(mode,process.cwd(), '')
+  return {
+    define: {
+      'process.env.EXPRESS_APP_SERVER_URL': JSON.stringify(env.EXPRESS_APP_SERVER_URL)
+    },
+    plugins: [react()]
+  }
 })
+
