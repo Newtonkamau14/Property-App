6384055ac9396995287f605e68ad9164683c6d78
Newtonkamau14 <newtonkamau14@gmail.com>
2024-11-16 12:59:46 +0300

    refactor failureRedirect to failureMessage

--- server/routes/auth.router.ts
+++ server/routes/auth.router.ts
@@ -1,27 +1,23 @@
 import { Router } from "express";
 import authController from "../controllers/auth.controller";
-import passport from "passport";
+import { passport } from "../config/passport";
 
 const router = Router();
 
 //Admin
-router
-  .route("/auth/admin/login")
-  .post(
-    passport.authenticate("login", {
-      successRedirect: "/admin",
-      failureRedirect: "/auth/admin/login",
-    })
-  );
+router.route("/admin/login").post(
+  passport.authenticate("login", {
+    successRedirect: "/admin",
+    failureMessage: "Failed to signup"
+  })
+);
 
-router
-  .route("/auth/admin/signup")
-  .post(
-    passport.authenticate("signup-admin", {
-      successRedirect: "/admin",
-      failureRedirect: "/auth/admin/signup",
-    })
-  );
+router.route("/admin/signup").post(
+  passport.authenticate("signup-admin", {
+    successRedirect: "/admin",
+    failureMessage: "Failed to login"
+  })
+);
 
 router.route("/logout").get(authController.logOutAdmin);
 
