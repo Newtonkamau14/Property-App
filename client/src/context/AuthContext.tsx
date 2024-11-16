6f487d168d8e328f268ae5377323d4caa95bb242
Newtonkamau14 <newtonkamau14@gmail.com>
2024-11-16 13:00:36 +0300

    add AuthContext logic

---
+++ client/src/context/AuthContext.tsx
@@ -0,0 +1,56 @@
+import React, { createContext, ReactNode, useEffect, useReducer } from "react";
+import { IUser } from "../models/user";
+
+type AuthState = {
+  user: IUser | null;
+};
+
+type AuthAction =
+  | { type: "LOGIN"; payload: IUser }
+  | { type: "LOGOUT"; payload: null };
+
+type AuthContextType = {
+  state: AuthState;
+  dispatch: React.Dispatch<AuthAction>;
+};
+
+export const AuthContext = createContext<AuthContextType | null>(null);
+
+export const authReducer = (
+  state: AuthState,
+  action: AuthAction
+): AuthState => {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        user: action.payload,
+      };
+
+    case "LOGOUT":
+      return {
+        user: null,
+      };
+    default:
+      return state;
+  }
+};
+
+export const AuthContextProvider: React.FC<{children: ReactNode}> = ({
+    children,
+}) => {
+    const [state,dispatch] = useReducer(authReducer,{user:null});
+
+    useEffect(() => {
+        const userString = localStorage.getItem('user')
+        if(userString){
+            const user: IUser = JSON.parse(userString)
+            dispatch({type: "LOGIN",payload:user})
+        }
+    },[])
+
+    return (
+        <AuthContext.Provider value={{state,dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
