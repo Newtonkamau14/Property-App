e5eab622ed12c148dee80865ea35440e33a472e5
Newtonkamau14 <newtonkamau14@gmail.com>
2024-11-16 13:00:54 +0300

    add AuthContext to main.tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,11 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { PropertyContextProvider } from "./context/PropertyContext.tsx";
+import { AuthContextProvider } from "./context/AuthContext.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <PropertyContextProvider>
-      <App />
-    </PropertyContextProvider>
+    <AuthContextProvider>
+      <PropertyContextProvider>
+        <App />
+      </PropertyContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
