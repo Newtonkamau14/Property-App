d64a4f44109e547722eb91dea2096620f12e1e83
Newtonkamau14 <newtonkamau14@gmail.com>
2024-11-20 13:33:56 +0300

    rename models dir to types

--- client/src/types/property.ts
+++ client/src/types/property.ts
@@ -1,9 +1,10 @@
 export type IProperty = {
+  property_id: string;
   property_name: string;
   property_type: string;
   property_location: string;
   property_price: string;
-  property_image: Buffer;
+  property_image: string;
   property_purpose: string;
   longitude: string;
   latitude: string
