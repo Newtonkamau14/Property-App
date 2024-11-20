f6adc908385d0f876500b118cafbfe1980164144
Newtonkamau14 <newtonkamau14@gmail.com>
2024-11-20 14:05:26 +0300

    add handleSubmit function

--- client/src/components/Signup.tsx
+++ client/src/components/Signup.tsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import { useSignUp } from "../hooks/useSignUp";
 
 function Signup() {
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const { signUp, error, isLoading } = useSignUp();
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    await signUp(username, email, password);
+  };
+
   return (
     <div className="d-flex align-items-center justify-content-center vh-100">
-      <form
-        action="/auth/admin/signup"
-        method="post"
-        className="border rounded p-3 col-4"
-      >
+      <form onSubmit={handleSubmit} className="border rounded p-3 col-4">
         <h3 className="text-center text-primary">Create Account</h3>
         <div className="form-group my-4">
           <label htmlFor="username">Username</label>
@@ -17,6 +26,8 @@ function Signup() {
             id="username"
             name="username"
             placeholder="Enter username"
+            onChange={(e) => setUsername(e.target.value)}
+            value={username}
           />
         </div>
         <div className="form-group my-4">
@@ -28,6 +39,8 @@ function Signup() {
             name="email"
             placeholder="Enter email"
             required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
           />
         </div>
         <div className="form-group my-4">
@@ -38,11 +51,23 @@ function Signup() {
             id="password"
             name="password"
             placeholder="Password"
+            onChange={(e) => setPassword(e.target.value)}
+            value={password}
           />
         </div>
-        <button type="submit" className="btn btn-primary my-2">
+        <button
+          type="submit"
+          className="btn btn-primary my-2"
+          disabled={isLoading}
+        >
           Sign Up
         </button>
+        {error && (
+          <div className="alert alert-danger" role="alert">
+            {error}
+          </div>
+        )}
+
         <div>
           <p>
             Already have an account.
